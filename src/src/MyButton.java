d862e609daa35efc6fcb83686b182a2a50f43c37
Мария <kudyan.masha@gmail.com>
2025-04-11 12:12:01 +0300

    11.04.25

--- src/src/MyButton.java
+++ src/src/MyButton.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+
 public class MyButton extends JButton {
     protected int x;
     protected int y;
@@ -8,9 +10,11 @@ public class MyButton extends JButton {
         this.x = i;
         this.y = j;
         this.znn = z;
+        Color d=new Color(238,238,238);
+        super.setBackground(d);
     }
     public void setValue(int value){
-        znn = value;
+        this.znn = value;
         setText(String.valueOf(znn));
     }
     public boolean sravnenie(int a){
@@ -18,5 +22,35 @@ public class MyButton extends JButton {
     }
     public boolean sravn(MyButton my){
         return (this.znn==my.znn);
+    }
+    public void setBackground() {
+        Color cl=new Color(0,255,255);
+        super.setBackground(cl);
+    }
+    public void delBackground(){
+        Color cl=new Color(238,238,238);
+        super.setBackground(cl);
+    }
+    public int getkx(){
+        return this.x;
+    }
+    public int getky(){
+        return this.y;
+    }
+    public int getnznn(){
+        return this.znn;
+    }
+    public boolean getBackgr(){
+        boolean sost=false;
+        Color zv=super.getBackground();
+        if (zv.getRed()==0 && zv.getGreen()==255 && zv.getBlue()==255){
+            sost=true;
+        }
+        return sost;
+    }
+    public void Exchangeznn(MyButton a){
+        int t=this.znn;
+        this.znn=a.getnznn();
+        a.znn=t;
     }
 }
---
+++ src/src/PlayingField.java
@@ -0,0 +1,314 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PlayingField extends JFrame {
+    public static final int NotUserElement=1;
+    public static final int SpecialLine=7;
+    public static final int SpecialColumn=8;
+    public static final int SpecialElement=9;
+    public static final int NoElement=0;
+    public static final int minElement=2;
+    public static final int maxElement=6;
+
+    private int random(int min, int max) {//функция случайного числа из диапазона(разобраться с синтаксисом)
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
+
+    private MyButton[][] NewButt() {
+        MyButton[][] mybutt = new MyButton[9][9];
+        for (int i=0; i<9; i++) {
+            mybutt[0][i] = new MyButton(0, i, NotUserElement);
+        }
+        for (int i = 1; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                mybutt[i][j] = new MyButton(i, j, random(minElement, maxElement));
+            }
+        }
+        return mybutt;
+    }
+
+    private MyButton[][] mybuttons = NewButt();
+
+    public PlayingField() {
+        super("Prilogenie");
+        setSize(320, 320);
+        setLocation(100, 100);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // Вспомогательная панель
+        JPanel grid = new JPanel();
+        /*
+         * Первые два параметра конструктора GridLayout определяют количество
+         * строк и столбцов в таблице. Вторые 2 параметра - расстояние между
+         * ячейками по горизонтали и вертикали
+         */
+        GridLayout layout = new GridLayout(9, 9, 5, 12);
+        grid.setLayout(layout);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                mybuttons[i][j].setText(String.valueOf(mybuttons[i][j].znn));
+                grid.add(mybuttons[i][j]);
+                mybuttons[i][j].addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        MyButton btn = (MyButton) e.getSource();
+                        int x0 = btn.getkx();
+                        int y0 = btn.getky();
+                        int x1 = 0;
+                        int y1 = 0;
+                        boolean AnyButtonPressed = false;
+                        for (int i = 0; i < 9; i++) {
+                            for (int j = 0; j < 9; j++) {
+                                if (mybuttons[i][j].getBackgr()) {
+                                    x1=i;
+                                    y1=j;
+                                    AnyButtonPressed = true;
+                                }
+                            }
+                        }
+                        if (btn.sravnenie(NotUserElement) && !AnyButtonPressed) {}
+                        else if (!AnyButtonPressed) {
+                            btn.setBackground();
+                        }
+                        else if (x0==x1 && y0==y1) {
+                            btn.delBackground();
+                        }
+                        else if (x0==x1 && Math.abs(y0-y1)==1) {
+                            mybuttons[x1][y1].delBackground();
+                            mybuttons[x0][y0].Exchangeznn(mybuttons[x1][y1]);
+                            boolean veupol= check2y(mybuttons, y0) || check2y(mybuttons, y1) || check1y(mybuttons, x0);
+                            if (!veupol) {
+                                mybuttons[x0][y0].Exchangeznn(mybuttons[x1][y1]);
+                            }
+                            if (veupol) {
+                                rePaint();
+                            }
+                        }
+                        else if (y0==y1 && Math.abs(x0-x1)==1) {
+                            mybuttons[x1][y1].delBackground();
+                            mybuttons[x0][y0].Exchangeznn(mybuttons[x1][y1]);
+                            boolean veupol = check1y(mybuttons, x0) || check1y(mybuttons, x1) || check2y(mybuttons, y0);
+                            if (!veupol) {
+                                mybuttons[x0][y0].Exchangeznn(mybuttons[x1][y1]);
+                            }
+                            if (veupol) {
+                                rePaint();
+                            }
+                        }
+                    }
+                });
+
+            }
+        }
+        rePaint();
+        // Размещаем нашу панель в панели содержимого
+        getContentPane().add(grid);
+        // Устанавливаем оптимальный размер окна
+        pack();
+        // Открываем окно
+        setVisible(true);
+    }
+
+    private void check1(MyButton mas[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                int numberinarow = 0;
+                int linestart = 0;
+                if (mas[i][j - 1].sravn(mas[i][j]) && !mas[i][j].sravnenie(1)) {
+                    numberinarow = 2;
+                    linestart = j - 1;
+                    while (j < 8 && mas[i][j].sravn(mas[i][j + 1])) {
+                        numberinarow++;
+                        j++;
+                    }
+                    j = linestart + 1;
+                    switch (numberinarow) {
+                        case 2:
+                            break;
+                        case 3:
+                            for (int k = linestart; k < linestart + numberinarow; k++) {
+                                mas[i][k].setValue(NoElement);
+                            }
+                            break;
+                        case 4:
+                            for (int k = linestart + 1; k < linestart + numberinarow; k++) {
+                                mas[i][k].setValue(NoElement);
+                            }
+                            mas[i][linestart].setValue(SpecialColumn);
+                            break;
+                        default:
+                            for (int k = linestart + 1; k < linestart + numberinarow; k++) {
+                                mas[i][k].setValue(NoElement);
+                            }
+                            mas[i][linestart].setValue(SpecialElement);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    static void check2(MyButton mas[][]) {
+        for (int j = 0; j < 9; j++) {
+            for (int i = 1; i < 9; i++) {
+                int numberinarow = 0;
+                int linestart = 0;
+                if (mas[i - 1][j].sravn(mas[i][j]) && !mas[i][j].sravnenie(1)) {
+                    numberinarow = 2;
+                    linestart = i - 1;
+                    while (i < 8 && mas[i][j].sravn(mas[i + 1][j])) {
+                        numberinarow++;
+                        i++;
+                    }
+                    i = linestart + 1;
+                    switch (numberinarow) {
+                        case 2:
+                            break;
+                        case 3:
+                            for (int k = linestart; k < linestart + numberinarow; k++) {
+                                mas[k][j].setValue(NoElement);
+                            }
+                            break;
+                        case 4:
+                            for (int k = linestart; k < linestart + numberinarow - 1; k++) {
+                                mas[k][j].setValue(NoElement);
+                            }
+                            mas[linestart + numberinarow - 1][j].setValue(SpecialLine);
+                            break;
+                        default:
+                            for (int k = linestart; k < linestart + numberinarow - 1; k++) {
+                                mas[k][j].setValue(NoElement);
+                            }
+                            mas[linestart + numberinarow - 1][j].setValue(SpecialElement);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean check2y(MyButton mas[][], int t) {
+        for (int i = 1; i < 9; i++) {
+            int numberinarow = 0;
+            int linestart = 0;
+            if (mas[i - 1][t].sravn(mas[i][t]) && !mas[i][t].sravnenie(1)) {
+                numberinarow = 2;
+                linestart = i - 1;
+                while (i < 8 && mas[i][t].sravn(mas[i + 1][t])) {
+                    numberinarow++;
+                    i++;
+                }
+                i = linestart + 1;
+                switch (numberinarow) {
+                    case 2:
+                        return false;
+                    case 3:
+                        for (int k = linestart; k < linestart + numberinarow; k++) {
+                            mas[k][t].setValue(NoElement);
+                        }
+                        break;
+                    case 4:
+                        for (int k = linestart; k < linestart + numberinarow - 1; k++) {
+                            mas[k][t].setValue(NoElement);
+                        }
+                        mas[linestart + numberinarow - 1][t].setValue(SpecialLine);break;
+                    default:
+                        for (int k = linestart; k < linestart + numberinarow - 1; k++) {
+                            mas[k][t].setValue(NoElement);
+                        }mas[linestart + numberinarow - 1][t].setValue(SpecialElement);
+                        break;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean check1y(MyButton mas[][], int t) {
+        boolean result = false;
+        for (int j = 1; j < 9; j++) {
+            int numberinarow = 0;
+            int linestart = 0;
+            if (mas[t][j - 1].sravn(mas[t][j]) && !mas[t][j].sravnenie(1)) {
+                numberinarow = 2;
+                linestart = j - 1;
+                while (j < 8 && mas[t][j].sravn(mas[t][j + 1])) {
+                    numberinarow++;
+                    j++;
+                }
+                j = linestart + 1;
+                switch (numberinarow) {
+                    case 2:
+                        break;
+                    case 3:
+                        for (int k = linestart; k < linestart + numberinarow; k++) {
+                            mas[t][k].setValue(NoElement);
+                            result = true;
+                        }
+                        break;
+                    case 4:
+                        for (int k = linestart + 1; k < linestart + numberinarow; k++) {
+                            mas[t][k].setValue(NoElement);
+                        }
+                        mas[t][linestart].setValue(SpecialColumn);
+                        result = true;
+                        break;
+                    default:
+                        for (int k = linestart + 1; k < linestart + numberinarow; k++) {
+                            mas[t][k].setValue(NoElement);
+                        }
+                        mas[t][linestart].setValue(SpecialElement);
+                        result = true;
+                        break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public void rePaint() {
+        check1(mybuttons);
+        check2(mybuttons);
+        boolean marker = false;
+        while (!marker) {
+            for (int j = 0; j < 9; j++) {
+                for (int i = 0; i < 8; i++) {
+                    if (mybuttons[i + 1][j].sravnenie(0)) {
+                        mybuttons[i+1][j].Exchangeznn(mybuttons[i][j]);
+                        }
+                }
+            }
+            marker = true;
+            for (int j = 0; j < 9; j++) {
+                for (int i = 8; i > 0; i = i - 1) {
+                    if (mybuttons[i][j].sravnenie(0) && !mybuttons[i - 1][j].sravnenie(0)) {
+                        marker = false;
+                    }
+                }
+            }
+        }
+        int a = 2;
+        int b = 6;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (mybuttons[i][j].sravnenie(0)) {
+                    mybuttons[i][j].setValue(random(a, b));
+                }
+            }
+        }
+        check1(mybuttons);
+        check2(mybuttons);
+        boolean existsZeroInTable =false;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (mybuttons[i][j].sravnenie(0)) {
+                    existsZeroInTable = true;
+                }
+            }
+        }
+        if (existsZeroInTable) {
+            rePaint();
+        }
+    }
+}
